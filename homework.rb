f865d3e194b64f9d6c9b5bd70f15bb82e08d915b
Lam Bui Bao <lambuibao@outlook.com>
2020-10-26 21:11:35 +0700

    English homework

---
+++ homework.rb
@@ -0,0 +1,11 @@
+puts "What are you considering to buy? Notice, this is only a general recommendation."
+    puts "1. CPU + Motherboard"
+    puts "2. GPU"
+    puts "3. Storage solutions"
+    puts "4. Full PC"
+print "Your answer: "
+    answer = gets.chomp
+
+if answer == "1"
+    puts "What are you planning to do with this upgrade?"
+        puts ""
