ab3fe494e4135d5570018662ab3a72aa5971bd69
Lam Bui Bao <lambuibao@outlook.com>
2022-02-17 22:17:28 +0700

    Added a .gitignore and updated triangle files

---
+++ bai3/triangle.py
@@ -0,0 +1,24 @@
+print("This will check whether 3 numbers create a triangle.")
+print("This will calculate the parameters if they create a triangle.")
+
+var_a = float(input("What's the first number?"))
+var_b = float(input("What's the second number?"))
+var_c = float(input("What's the last number?"))
+
+
+class Verify:
+    validation1 = var_a + var_b > var_c
+    validation2 = var_a + var_c > var_b
+    validation3 = var_b + var_c > var_a
+
+    def Validate():
+        print(var_a, var_b, var_c)
+
+
+Verify.Validate()
+
+
+class Calculate:
+    perimeter = var_a + var_b + var_c
+    heronsquare = (perimeter / 2) * ((perimeter / 2) - var_a) * \
+        ((perimeter / 2) - var_b) * ((perimeter / 2) - var_c)
--- bai3/triangle.rb
+++ bai3/triangle.rb
@@ -5,10 +5,10 @@ puts "This application will check whether 3 random numbers create a triangle. If
 print "What's the first number? " 
 	var_a = gets.chomp.to_f # Takes in the first value and converts it to a float.
 
-print "What's the next number? "
+print "What's the second number? "
 	var_b = gets.chomp.to_f # Takes in the second value and converts it to a float.
 
-print "What's the next number? "
+print "What's the last number? "
 	var_c = gets.chomp.to_f # Takes in the third value and converts it to a float.
 
 class Verify
@@ -18,7 +18,7 @@ class Verify
 
 	def Validate
 		if @@validation1 == false or @@validation2 == false or @@validation3 == false # Checks in a long-ass fucking conditional. Why am I doing it like this again? Is there some way to shorten it? How do I even refactor this thing?
-			print "These sides do not create a triangle."
+			print "These numbers do not create a triangle."
 			exit!	
 		else
 			Calculate.new # This class instance will be defined underneath.
